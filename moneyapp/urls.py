dfc1528195b709473432edd9783e7c2653ec028b
Shamshodbek Khazratov <93393135+Rokki-Khazratov@users.noreply.github.com>
2024-01-25 16:16:36 +0500

    Update urls.py

--- moneyapp/urls.py
+++ moneyapp/urls.py
@@ -3,10 +3,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [ 
-    path('', index , ),
-    path('' , all_category , ),
-    path('' , add_new_consumption, ),
-    path('category/<int:Sub_category>' , category_detail ,  name="category_detail" ),
-    path('' , sub_category , ),
-
+    path('', index, name='index'),
+    path('all_category/', all_category, name='all_category'),
+    path('add_new_consumption/', add_new_consumption, name='add_new_consumption'),
+    path('category/<int:sub_category_id>/', category_detail, name='category_detail'),
+    path('sub_category/', sub_category, name='sub_category'),
 ]
