9d1c3f7d7b21b300b621bee8c7743add2b9071dc
“krv006” <rvkamronbek@gmail.com>
2024-02-26 19:14:34 +0500

    money

--- moneyapp/views.py
+++ moneyapp/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import get_object_or_404
 from .models import Category,Consumption,Sub_category,Card
 from .forms import ConsumptionForm
 from django.db.models import Sum
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login, authenticate
 
 # Create your views here.
 
@@ -69,3 +72,29 @@ def sub_category_detail(request, sub_category_id):
     }
 
     return render(request, 'sub_category_detail.html', context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                return redirect('index') 
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
